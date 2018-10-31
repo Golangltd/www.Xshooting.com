f2e454fa269c37055fc071cfae4c1a1406e3a0a3
cserli <1528884535@qq.com>
2018-10-31 15:29:23 +0800

    add data

---
+++ XSutil/xutil.go
@@ -0,0 +1,124 @@
+package xutil
+
+import (
+	"time"
+	"strconv"
+	"FenDZ/glog-master"
+	"LollipopGoFrame/root"
+	"LollipopGoFrame/Logic/player"
+	"xcode/XModule"
+	"net/http"
+	"xcode/xprofile"
+)
+
+// 版本
+var (
+	MarjoVersion string
+	MinorVersion string
+	PatchVersion string
+	GApp = Application{}
+)
+
+// 接口数据
+type App interface {
+	Run(f func(int64))
+	Init()bool
+	Distroy()
+}
+
+// 实现接口的结构
+type Application struct {
+	exit           bool
+	fps            int
+	tickTotal      int64
+	FrameTime      time.Duration
+	FrameTimeTotal time.Duration
+	pidFile        string
+	profile        *xprofile.Profiler
+	httpAddr       string
+	httpPort       int
+	serveMux       *http.ServeMux
+	//pattern        *pat.PatternServeMux
+	//http2main      chan *httpCmd
+	main2http      chan string
+	httpCmds       []string
+	InitOKTime     time.Time // 初始化完成计时
+	invokeOnMain   chan struct{}
+	invokeFunc     func()
+}
+
+func (this *Application)Init(f func(int64))bool{
+	root.DModule= XModule.NewDmodule(root.DLollipopGoMAX)
+	root.DModule.Register(root.DLollipopGoPlayer,player.NewPlayer())
+	flock,err:=NewFolck(this.pidFile)
+	if err!=nil{
+		glog.Info(err,flock)
+	}
+	this.profile.Start(this.pidFile)
+	return true
+}
+
+func (this *Application)Distroy(){
+}
+
+// 设置PID file
+func (this *Application)SetPidFile(pidFile string) *Application {
+	this.pidFile = pidFile
+	return this
+}
+
+// 设置fps
+func (this *Application)SetFps(needFps int) *Application {
+	if needFps <=0 || needFps >1000{
+		needFps = 1000
+	}
+	this.fps = needFps
+	return this
+}
+
+// 设置版本
+func (this *Application)SetVersion(major,minor,patch int) *Application {
+	MarjoVersion = strconv.Itoa(major)
+	MinorVersion = strconv.Itoa(minor)
+	PatchVersion = strconv.Itoa(patch)
+	return this
+}
+
+
+// 帧同步函数
+func (this *Application)Run(f func(int64))  {
+	t:=time.Duration(int64(time.Millisecond)*int64(1000))/20
+	lasttime :=time.Now().UnixNano()
+	var nowtime int64
+	ticker:=time.NewTicker(t)
+	defer  ticker.Stop()
+mainLoop:
+	for{
+		select {
+		case <- ticker.C:
+			if this.exit {
+				break mainLoop
+			}
+			nowtime=time.Now().UnixNano()
+			dt:=nowtime -lasttime
+			f(dt)
+			this.FrameTime = time.Duration(time.Now().UnixNano()-nowtime)
+			this.FrameTimeTotal+=this.FrameTime
+			if this.FrameTime >t && nowtime/int64(time.Millisecond)%1000<=50{
+				glog.Warning(" FPS 高于 50 ")
+			}
+			lasttime = nowtime
+			this.tickTotal++
+		//case cmd := <-this.http2main:
+		//	ret := cmd.cmdFunc(cmd.args)
+		//	select {
+		//	case this.main2http <- ret:
+		//	default:
+		//	}
+		case <-this.invokeOnMain: // 网路事件处理 ，事件驱动处理  ； 0-50ms 数据处理
+			if this.invokeFunc != nil {
+				this.invokeFunc()
+			}
+		}
+	}
+}

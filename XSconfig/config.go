f12eb873f3a8e3e5b5c724b0f687e92c9456cff4
cserli <1528884535@qq.com>
2018-11-01 16:39:36 +0800

    Update XSconfig/config.go

--- XSconfig/config.go
+++ XSconfig/config.go
@@ -1,5 +1,14 @@
 package Config
 
+// 配置文件结构信息
+type Config struct {
+	ID   string // ID 序号信息
+	Name string // 名字
+	IP   string // 服务器IP
+	Port string // 服务器端口
+	Dsp  string // 备注信息
+}
+
 // 获取配置文件
 func init() {
 
